9afea7fcc8f59d4bde929eba5024e9e3749d7c1e
RedCElec <115882781+RedCElec@users.noreply.github.com>
2023-06-07 15:16:00 +0200

    Update Test.py

--- Test.py
+++ Test.py
@@ -1,17 +1,16 @@
 import time
 from PIL import Image, ImageDraw, ImageFont
 import Adafruit_SSD1306
+import board
 
 # OLED display configuration
 DISPLAY_WIDTH = 128
 DISPLAY_HEIGHT = 64
-DISPLAY_ADDRESS = 0x3C  # Check your display address using `sudo i2cdetect -y 1`
+DISPLAY_ADDRESS = 0x3C
 
-# Initialize the OLED display
-display = Adafruit_SSD1306.SSD1306_128_64(rst=None, i2c_address=DISPLAY_ADDRESS)
-display.begin()
-display.clear()
-display.display()
+# Initialize the I2C bus
+i2c = board.I2C()
+display = Adafruit_SSD1306.SSD1306_I2C(DISPLAY_WIDTH, DISPLAY_HEIGHT, i2c, addr=DISPLAY_ADDRESS)
 
 # Create a blank image with a black background
 image = Image.new("1", (DISPLAY_WIDTH, DISPLAY_HEIGHT))
@@ -28,7 +27,7 @@ def display_message(message):
 
     # Display the image
     display.image(image)
-    display.display()
+    display.show()
 
 # Main loop
 if __name__ == '__main__':
@@ -39,5 +38,5 @@ if __name__ == '__main__':
             time.sleep(5)  # Delay between updates
 
     except KeyboardInterrupt:
-        display.clear()
-        display.display()
+        display.fill(0)
+        display.show()
