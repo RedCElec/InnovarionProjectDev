e4a1a5f145f4e94a4ce334cd22182ab4448c5f2e
RedCElec <115882781+RedCElec@users.noreply.github.com>
2023-06-08 11:49:20 +0200

    Update Launch.sh

--- Launch.sh
+++ Launch.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
 # Launch C++ program
-cd ~/Desktop/innov/src/
+cd ~/Desktop/innov/InnovarionProjectDev/src/
 make
 ./tester_notBuild &
 
+cpp_program_pid=$!
+
 # Launch web server
-cd ~/Desktop/innov/webserver/
-npm run dev > ~/Desktop/innov/webserver/logfile.txt 2>&1 &
+cd ~/Desktop/innov/InnovarionProjectDev/webserver/
+npm run dev > ~/Desktop/innov/InnovarionProjectDev/webserver/log.txt 2>&1 &
 
 echo "Web server launched. Press Ctrl+C to stop."
-
-tail -f ~/Desktop/innov/webserver/logfile.txt
+trap "kill $cpp_program_pid" INT
+tail -f ~/Desktop/innov/InnovarionProjectDev/webserver/log.txt
